2c876545c9e33efc9909c34539cd02bc3786ae30
unknown <adenijidamilola2@gmail.com>
2020-05-25 11:28:58 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//B CONNECTION: mongodb+srv://Dami:<password>@cluster0-teywi.mongodb.net/test?retryWrites=true&w=majority
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+const userRoutes = require('./routes/user');
+const app = express();
+
+mongoose.connect('mongodb+srv://Dami:damilola2@cluster0-teywi.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  app.post('/api/stuff', (req, res, next) => {
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'My first thing',
+        description: 'All of the info about my first thing',
+        imageUrl: '',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'My second thing',
+        description: 'All of the info about my second thing',
+        imageUrl: '',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json();
+  });
+  app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
---
+++ controllers/stuff.js
@@ -0,0 +1,40 @@
+// in controllers/stuff.js
+
+const Thing = require('../models/thing');
+
+exports.createThing = (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+module.exports = router;
