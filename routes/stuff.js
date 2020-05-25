26fc0ac9b8798c42ec7a5ff99a54a90bab6755dc
unknown <adenijidamilola2@gmail.com>
2020-05-25 12:01:45 +0100

    done commit

--- routes/stuff.js
+++ routes/stuff.js
@@ -106,8 +106,6 @@ router.get('/' +
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-const stuffCtrl = require('../controllers/stuff');
-
 router.get('/', auth, stuffCtrl.getAllStuff);
 router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
